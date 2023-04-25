aa750a1dab63f5103433ebe39d074b3bae29ded3
Varsha Jolly <vjk@Varshas-MacBook-Air.local>
2023-04-24 23:11:54 -0600

    added circleci config file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import jsonpickle
 import requests
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -13,38 +14,37 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/analyze/wordcloud")
-def get_wordcloud():
+@app.route("/analyze/<topic>/wordcloud")
+def get_wordcloud(topic):
+    tweets = DbConnect(topic)
     tweets_df = pd.DataFrame(columns=['preprocessed_tweets'])
     for data in tweets:
         index = len(tweets_df)
         tweets_df.loc[index, 'preprocessed_tweets'] = pre_process(data[0])
     img = keywords_cloud(tweets_df)
-    # imgword = 'data:image/png;base64,{}'.format(base64.b64encode(img.getvalue()).decode())
-    # return render_template('visuals.html', wordcloud=imgword)
     img.save("./wordcloud.png")
     return send_file('./wordcloud.png')
 
-@app.route("/analyze/sentiment")
-def get_sentiment():
+@app.route("/analyze/<topic>/sentiment")
+def get_sentiment(topic):
+    tweets = DbConnect(topic)
     sentiment_list = sentiment(tweets)
     return Response(response=jsonpickle.encode(sentiment_list), status=200, mimetype="application/json")
 
 @app.route("/analyze/<topic>")
 def analyze(topic):
-    # loc_url = "http://10.0.0.99:7007/collector/{}".format(topic)
+    # collector_url = os.getenv("COLLECTOR_URL")
+    # loc_url = "{0}/collector/{1}".format(collector_url,topic)
     # result = requests.get(loc_url).json()
     # while "status" not in result:
     #     result = requests.get(loc_url).json()
     # if result["status"] == "success":
     #     print("success")
-    # topic = "india"
-    global tweets
-    tweets = DbConnect(topic)
-    # print(tweets)
+    # global tweets
+    # tweets = DbConnect(topic)
     result = {"status":"success"}
     return Response(response=jsonpickle.encode(result), status=200, mimetype="application/json")
 
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=8000)
-    # app.run()
+#     app.run()
