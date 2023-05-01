4c9d2fd527758099329bf447b0d4df4fb7aad440
Varsha Jolly <vjk@Varshas-MacBook-Air.local>
2023-05-01 17:51:31 -0600

    testing api using pytest-mock

--- test/test_routes.py
+++ test/test_routes.py
@@ -5,7 +5,7 @@ def test_home_route(app, client):
     response = client.get('/')
     assert response.status_code == 200
 
-def test_analyze_route(app, client, topic, mock_api_response):
+def test_analyze_route(app, client, topic):
     response = client.get('/analyze/{}'.format(topic))
     res = json.loads(response.data.decode('utf-8'))
     assert type(res) is dict
