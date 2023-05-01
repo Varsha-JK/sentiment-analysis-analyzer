f32de64fc1df7dbf4e247896eb64500f5804ac3c
Varsha Jolly <vjk@Varshas-MacBook-Air.local>
2023-05-01 17:43:36 -0600

    testing api using pytest-mock

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,26 @@
 import pytest
 from app import app as backend_app
+from unittest.mock import Mock
+
 
 @pytest.fixture
 def app():
     yield backend_app
 
+
 @pytest.fixture
 def client(app):
     return app.test_client()
 
+
 @pytest.fixture
 def topic():
     yield "music"
+
+
+@pytest.fixture
+def mock_api_request(mocker):
+    mock = Mock(status_code=200)
+    mock.json.return_value = {"status":200}
+    mocker.patch("requests.get", return_value=mock)
+    return mock
--- test/test_routes.py
+++ test/test_routes.py
@@ -1,15 +1,16 @@
 import json
 
+
 def test_home_route(app, client):
     response = client.get('/')
     assert response.status_code == 200
 
-# def test_analyze_route(app, client, topic):
-#     response = client.get('/analyze/{}'.format(topic))
-#     res = json.loads(response.data.decode('utf-8'))
-#     assert type(res) is dict
-#     assert res['status'] == 'success'
-#     assert response.status_code == 200
+def test_analyze_route(app, client, topic, mock_api_response):
+    response = client.get('/analyze/{}'.format(topic))
+    res = json.loads(response.data.decode('utf-8'))
+    assert type(res) is dict
+    assert res['status'] == 'success'
+    assert response.status_code == 200
 
 
 def test_sentiment_route(app, client, topic):
